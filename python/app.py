291177d03291065ffcd6c3fb8b9c6a3ece22c334
dd-shocnt <123931421+dd-shocnt@users.noreply.github.com>
2023-10-03 15:14:51 +0900

    Update app.py

--- python/app.py
+++ python/app.py
@@ -2,9 +2,10 @@ import os
 from flask import Flask, Response
 
 app = Flask(__name__)
+msg = "Hello world from su-cloudrun-python " + os.environ.get("DD_VERSION")
 
 @app.route("/")
 def home():
-    return Response("{\"msg\":\"Hello world from su-cloudrun-python　os.environ.get("DD_VERSION")!\"}", status=200, mimetype='application/json')
+    return Response("{\"msg\": msg}", status=200, mimetype='application/json')
 
 app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
